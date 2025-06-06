825a943e02463afb89067447843b21fb52415025
kelikovic <kkinyama@zesco.co.zm>
2025-06-06 13:45:22 +0200

    Update sdp_offer2.dart

--- lib/src/sdp8/sdp_offer2.dart
+++ lib/src/sdp8/sdp_offer2.dart
@@ -747,3 +747,70 @@ void main() {
 
 //   print("Sdp offer: $offer");
 // }
+Map<String, dynamic> sdpOfferWithSingleCandidate = {
+  'version': 0,
+  'origin': {
+    'username': '-',
+    'sessionId': 4215775240449105457,
+    'sessionVersion': 2,
+    'netType': 'IN',
+    'ipVer': 4,
+    'address': '0.0.0.0',
+  },
+  'name': '-',
+  'timing': {
+    'start': 0,
+    'stop': 0,
+  },
+  'media': [
+    {
+      'rtp': [
+        {
+          'payload': 8,
+          'codec': 'PCMA',
+          'rate': 8000,
+          'encoding': null,
+        }
+      ],
+      'fmtp': [],
+      'type': 'audio',
+      'port': 4444,
+      'protocol': 'UDP/TLS/RTP/SAVPF',
+      'payloads': '111 63 9 0 8 13 110 126',
+      'connection': {
+        'version': 4,
+        'ip': '0.0.0.0',
+      },
+      'setup': 'passive',
+      'mid': 0,
+      'iceUfrag': 'yxYb',
+      'icePwd': '05iMxO9GujD2fUWXSoi0ByNd',
+      'fingerprint': {
+        'type': 'sha-256',
+        'hash': '2B:B7:82:67:53:4B:0C:35:52:57:0D:77:62:2A:B3:1F:BA:57:C0:FB:1C:F6:7A:19:E6:9C:F8:15:D1:C3:54:F1',
+      },
+      'rtcpMux': 'rtcp-mux',
+      'candidates': [
+        {
+          'foundation': 1,
+          'component': 1,
+          'transport': 'udp',
+          'priority': 2113937151,
+          'ip': '192.168.56.1',
+          'port': 4444,
+          'type': 'host',
+          'raddr': null,
+          'rport': null,
+          'tcptype': null,
+          'generation': null,
+          'network-id': null,
+          'network-cost': null,
+        }
+      ],
+    }
+  ],
+};
+
+// You can print it to verify
+// import 'dart:convert';
+// print(jsonEncode(sdpOfferWithSingleCandidate));
